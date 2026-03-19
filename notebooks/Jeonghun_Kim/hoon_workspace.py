695e5e765b204fc54d2de819ab5bfb992cada4df
KimJeongHun <kks1182415@gmail.com>
2026-03-20 01:48:50 +0900

    선형회귀

--- notebooks/Jeonghun_Kim/hoon_workspace.py
+++ notebooks/Jeonghun_Kim/hoon_workspace.py
@@ -1,116 +1,42 @@
-import math
 import matplotlib.pyplot as plt
 
-#1. 활성화함수와 미분
-def sigmoid(x):
-    return 1/(1+math.exp(-x))
+x = [1, 2, 3, 4, 5]
+y = [3, 5, 7, 9, 11]
 
-def sigmoid_derivative(x):
-    s = sigmoid(x)
-    return s * (1-s)
+w = 1.0
+b = 3.0
 
-# 2. 입력값, 정답, 초기 파라미터
-x = 2.0
-y = 1.0
+learning_rate = 0.01
+epochs = 1000
 
-w1 = 0.5
-b1 = 0.0
-w2 = -0.3
-b2 = 0.0
+n = len(x)
+loss_history = []
 
-learning_rate = 0.1
-epochs = 100
-
-loss_history=[] 
-
-# 3. 순전파 (forward propagation)
-'''
-z1 = w1*x+b1
-a1 = sigmoid(z1)
-z2 = w2*a1+b2
-y_hat = z2
-loss = 0.5*(y_hat-y)**2
-'''
+y_hat=[0]*n
 for epoch in range(epochs):
-    z1 = w1*x+b1
-    a1 = sigmoid(z1)
-    z2 = w2*a1+b2
-    y_hat = z2
-    loss = 0.5*(y_hat-y)**2
+    tmp1=0
+    tmp2=0
+    tmp3=0
+    for i in range(n):
+        y_hat[i]=w*x[i]+b
 
-    loss_history.append(loss)
+    for i in range(n):
+        tmp1 += (y_hat[i]-y[i])**2
 
-    dL_dyhat = y_hat - y
+    loss_history.append(tmp1/n)
 
-    # y_hat = z2 이므로
-    dL_dz2 = dL_dyhat
+    for i in range(n):
+        tmp2 += (y_hat[i]-y[i])*x[i]
+    dL_dw = 2*tmp2/n
 
-    # z2 = w2 * a1 + b2
-    dL_dw2 = dL_dz2 * a1
-    dL_db2 = dL_dz2
-    dL_da1 = dL_dz2 * w2
+    for i in range(n):
+        tmp3 += (y_hat[i]-y[i])
+    dL_db = 2*tmp3/n
 
-    # a1 = sigmoid(z1)
-    dL_dz1 = dL_da1 * sigmoid_derivative(z1)
+    #print(w, b)
+    w = w-learning_rate*dL_dw
+    b = b-learning_rate*dL_db
 
-    # z1 = w1 * x + b1
-    dL_dw1 = dL_dz1 * x
-    dL_db1 = dL_dz1
-
-    # 5. 경사하강법 업데이트
-    w1 = w1 - learning_rate * dL_dw1
-    b1 = b1 - learning_rate * dL_db1
-    w2 = w2 - learning_rate * dL_dw2
-    b2 = b2 - learning_rate * dL_db2
-'''
-print("=== Forward ===")
-print(f"z1 = {z1:.6f}")
-print(f"a1 = {a1:.6f}")
-print(f"z2 = {z2:.6f}")
-print(f"y_hat = {y_hat:.6f}")
-print(f"loss = {loss:.6f}")
-'''
-'''
-# 4. 역전파 (backpropagation)
-# 출력층 오차
-dL_dyhat = y_hat - y
-
-# y_hat = z2 이므로
-dL_dz2 = dL_dyhat
-
-# z2 = w2 * a1 + b2
-dL_dw2 = dL_dz2 * a1
-dL_db2 = dL_dz2
-dL_da1 = dL_dz2 * w2
-
-# a1 = sigmoid(z1)
-dL_dz1 = dL_da1 * sigmoid_derivative(z1)
-
-# z1 = w1 * x + b1
-dL_dw1 = dL_dz1 * x
-dL_db1 = dL_dz1
-'''
-'''
-print("\n=== Backward ===")
-print(f"dL/dw2 = {dL_dw2:.6f}")
-print(f"dL/db2 = {dL_db2:.6f}")
-print(f"dL/dw1 = {dL_dw1:.6f}")
-print(f"dL/db1 = {dL_db1:.6f}")
-'''
-'''
-# 5. 경사하강법 업데이트
-w1 = w1 - learning_rate * dL_dw1
-b1 = b1 - learning_rate * dL_db1
-w2 = w2 - learning_rate * dL_dw2
-b2 = b2 - learning_rate * dL_db2
-'''
-'''
-print("\n=== Updated Parameters ===")
-print(f"w1 = {w1:.6f}")
-print(f"b1 = {b1:.6f}")
-print(f"w2 = {w2:.6f}")
-print(f"b2 = {b2:.6f}")
-'''
 plt.figure(figsize=(8, 5))
 plt.plot(loss_history, marker='o')
 plt.xlabel("Epoch")
@@ -118,3 +44,11 @@ plt.ylabel("Loss")
 plt.title("Loss over epochs")
 plt.grid(True)
 plt.show()
+
+
+    
+
+    
+    
+
+
